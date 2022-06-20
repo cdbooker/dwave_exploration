d39c18dd5339bba2d304cdbf618eaac50fe3efa2
cdb <cameron.booker@smithinst.co.uk>
2022-06-20 15:07:30 +0100

    Renaming files

--- dwave_solve.py
+++ dwave_solve.py
@@ -1,5 +1,6 @@
 
 import dimod
+import numpy as np
 from neal import SimulatedAnnealingSampler
 from dwave.system import LeapHybridCQMSampler
 
@@ -10,6 +11,7 @@ def load_constrained_quadratic_model(filename):
     print(cqm.variables)
     return cqm
 
+
 def solve_constrained_quadratic_model(cqm):
     sampler = LeapHybridCQMSampler()
     sampleset = sampler.sample_cqm(cqm)
@@ -17,18 +19,21 @@ def solve_constrained_quadratic_model(cqm):
     feasible_energies = []
     for sample, E in sampleset.data(fields=['sample', 'energy']):
         # Check all solutions for feasibility
-        if cqm.check_feasible(sample) == 0:
+        if cqm.check_feasible(sample):
             feasible_samples.append(sample)
             feasible_energies.append(E)
+    optimal_val = np.array(feasible_energies).min()
+    optimal_sol = feasible_samples[np.array(feasible_energies).argmin()]
     info = sampleset.info
-    return feasible_samples, info
+    return optimal_val, optimal_sol, info
 
 
 if __name__ == '__main__':
     file = 'test_problem.lp'
     model = load_constrained_quadratic_model(file)
 
-    feasible_solutions, solver_info = solve_constrained_quadratic_model(model)
-    print(feasible_solutions)
+    optimal_value, optimal_solution, solver_info = solve_constrained_quadratic_model(model)
+    print(optimal_value)
+    print(optimal_solution)
 
 
